c7d9f25bc4cb0eb968df18b3eeb00302036f091b
laboon <laboon@gmail.com>
2017-04-27 15:11:54 -0400

    added system settings

---
+++ src/com/laboon/SystemSettings.java
@@ -0,0 +1,28 @@
+package com.laboon;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
+import java.util.*;
+import javax.swing.*;
+
+public class SystemSettings {
+
+    public static MainWindow _mainWindow = null;
+
+    public static MainPanel _mainPanel = null;
+
+    
+    public static boolean _timer = false;
+    
+    public static boolean _checkSyntax = false;
+
+    public static boolean _checkEndOpcode = false;
+
+    public static boolean _viewProgramArea = true;
+
+    public static boolean _viewProgramStack = true;
+
+    public static boolean _viewProgramOutput = true;
+
+}
